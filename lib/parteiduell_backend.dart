69d4ecaea8e19b6ea80dd5eee26e71c82a5a8c6b
redsolver <info@redsolver.net>
2019-09-28 13:02:44 +0200

    QuizFrage ausgelagert

--- lib/parteiduell_backend.dart
+++ lib/parteiduell_backend.dart
@@ -1,34 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
 
-class QuizFrage {
-  String frage;
-  List<String> antworten;
-  String richtigeAntwort;
-  String thema;
-  String kontext;
-  String quelle;
+import 'models/quizfrage.dart';
 
-  QuizFrage(this.frage, this.antworten, this.richtigeAntwort,
-      {this.thema, this.kontext});
-
-  QuizFrage.fromJson(Map<String, dynamic> json)
-      : frage = json['frage'] ?? '',
-        antworten = json['antworten'].cast<String>() ?? [],
-        richtigeAntwort = json['richtigeAntwort'] ?? '',
-        thema = json['thema'] ?? '',
-        kontext = json['kontext'] ?? '',
-        quelle = json['quelle'] ?? '';
-
-  Map<String, dynamic> toJson() => {
-        'frage': frage,
-        'antworten': antworten,
-        'richtigeAntwort': richtigeAntwort,
-        'thema': thema,
-        'kontext': kontext,
-        'quelle': quelle,
-      };
-}
 
 List<QuizFrage> quizFragen;
 
@@ -47,6 +21,7 @@ run() async {
   await for (var request in server) {
     execute(request);
   }
+  
 }
 
 // Bearbeitung der Anfragen
